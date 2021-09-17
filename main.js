0e7dfb260eccb2844cba6c1f27efbc97b4f27208
Jayson Valdez <90065578+Jvaldez8@users.noreply.github.com>
2021-09-17 14:24:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var myFirstName = ("Jayson")
+var myLastName = ("Valdez")
+var myAge = ("24")
+
+
+alert("my first name is Jayson")
+alert("my last name is Valdez")
+alert("my age is 24 years old")
+
+console.log('This class is pushing me to study hard'); 
